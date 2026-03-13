9c439480210d62010e66122e2b08e53330625a37
tathodgauri55-cell <tathodgauri55@gmail.com>
2026-03-13 21:48:28 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,19 +1,23 @@
-const products=[
+const products = [
+
 {
 name:"LED Series Light",
 price:120,
 image:"images/light1.jpg"
 },
+
 {
 name:"Leaf Light",
 price:180,
 image:"images/light2.jpg"
 },
+
 {
 name:"Wall Light",
 price:350,
-image:"images/light3.jpg"
+image:"images/wall-light.jpg"
 }
+
 ];
 
 let html="";
@@ -21,9 +25,13 @@ let html="";
 products.forEach(p=>{
 html+=`
 <div class="product">
+
 <img src="${p.image}" width="150">
+
 <h3>${p.name}</h3>
+
 <p>Price ₹${p.price}</p>
+
 </div>
 `;
 });
